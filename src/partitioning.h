e7ddf67887d4fdd7762cdd4bb6f329991d418d53
Matthias Vallentin <vallentin@icir.org>
2013-07-10 11:44:07 +0200

    Document partitioning functions.

--- src/partitioning.h
+++ src/partitioning.h
@@ -3,6 +3,10 @@
 
 namespace bf {
 
+/// Maps a vector of digests into *k* partitions of the available cells.
+/// @param digests The *k* digests to partition
+/// @param cells The number cells to digest the *k* digests into.
+/// @return The indices corresponding to *digests*.
 std::vector<size_t> partition(std::vector<digest> const& digests, size_t cells)
 {
   assert(cells % digests.size() == 0);
@@ -13,6 +17,10 @@ std::vector<size_t> partition(std::vector<digest> const& digests, size_t cells)
   return indices;
 };
 
+/// Maps a vector of digests uniformly to indices using modulus.
+/// @param digests The *k* digests to partition
+/// @param cells The number cells to digest the *k* digests into.
+/// @return The indices corresponding to *digests*.
 std::vector<size_t> mod(std::vector<digest> const& digests, size_t cells)
 {
   std::vector<size_t> indices(digests.size());
