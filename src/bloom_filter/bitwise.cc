49632e7a7479ef9f08b5e96015a4cedbdda9176b
Matthias Vallentin <vallentin@icir.org>
2013-07-17 15:46:15 +0200

    Fix addition in bitwise BF.

--- src/bloom_filter/bitwise.cc
+++ src/bloom_filter/bitwise.cc
@@ -10,26 +10,28 @@ bitwise_bloom_filter::bitwise_bloom_filter(size_t k, size_t cells, size_t seed)
 
 void bitwise_bloom_filter::add(object const& o)
 {
-  size_t i = 0;
-  while (i != levels_.size())
-  {
-    if (! levels_[i].lookup(o))
-      break;
-    // FIXME: do not hash element more than once.
-    levels_[i++].remove(o);
-  }
+  size_t l = 0;
+  // FIXME: do not hash element more than once for better performance.
+  while (l < levels_.size())
+    if (levels_[l].lookup(o))
+    {
+      levels_[l++].remove(o);
+    }
+    else
+    {
+      levels_[l].add(o);
+      return;
+    }
 
-  if (i == levels_.size())
-    grow();
-
+  grow();
   levels_.back().add(o);
 }
 
 size_t bitwise_bloom_filter::lookup(object const& o) const
 {
   size_t result = 0;
-  for (size_t i = 0; i < levels_.size(); ++i)
-    result += levels_[i].lookup(o) << i;
+  for (size_t l = 0; l < levels_.size(); ++l)
+    result += levels_[l].lookup(o) << l;
   return result;
 }
 
@@ -44,8 +46,8 @@ void bitwise_bloom_filter::grow()
   auto l = levels_.size();
 
   // TODO: come up with a reasonable growth scheme.
-  static size_t const min_size = 1024;
-  auto cells = cells_ / (2 * l);
+  static size_t const min_size = 128;
+  auto cells = l == 0 ? min_size : cells_ / (2 * l);
   if (cells < min_size)
     cells = min_size;
 
@@ -54,7 +56,7 @@ void bitwise_bloom_filter::grow()
   for (size_t i = 0; i < l; ++i)
     seed = prng();
 
-  levels_.emplace_back(k_, cells, seed);
+  levels_.emplace_back(make_hasher(k_, seed), cells);
 }
 
 } // namespace bf
